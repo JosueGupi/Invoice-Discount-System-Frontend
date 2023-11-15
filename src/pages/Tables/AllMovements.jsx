4b6473cec70b12447fca712b415f467330e471f7
Valeria Chinchilla <vchinchilla02@hotmail.com>
2023-11-14 18:37:55 -0600

    add table

--- src/pages/Tables/AllMovements.jsx
+++ src/pages/Tables/AllMovements.jsx
@@ -1,9 +1,10 @@
 import React, { Fragment, useEffect, useState, useRef } from 'react'
+
+import './Table.css';
+
 import axios from 'axios'
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-
-
 
 
 export function AllMovements() {
@@ -41,7 +42,33 @@ export function AllMovements() {
                     <br />
                     <button className='back-button' onClick={goToDataMenu}>Atras</button>
 
-                    <h1>hola</h1>
+                    <div>
+                        <h2>Tabla de Movimientos</h2>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>No operación</th>
+                                    <th>Fecha movimiento</th>
+                                    <th>Código contable</th>
+                                    <th>Cliente</th>
+                                    <th>Monto</th>
+                                    <th>Tipo movimiento</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {dataR.map((item, index) => (
+                                    <tr key={index}>
+                                        <td>{item.noOperacion}</td>
+                                        <td>{item.fechaMovimiento}</td>
+                                        <td>{item.codigoContable}</td>
+                                        <td>{item.cliente}</td>
+                                        <td>{item.monto}</td>
+                                        <td>{item.tipoMovimiento}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
 
                 </div>
             </div>
