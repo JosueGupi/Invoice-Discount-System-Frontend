bd135f39465313f6f79951843d0feff6e7d1641b
lalicas <hilaryc.cr@estudiantec.cr>
2023-11-16 15:09:55 -0600

    Filter display change

--- src/pages/Tables/AllMovements.jsx
+++ src/pages/Tables/AllMovements.jsx
@@ -22,6 +22,8 @@ export function AllMovements() {
             try {
                 const response = await axios.get('https://inversiones-ellens-7b3ebbfa2822.herokuapp.com/tables/getTableMovements');
                 setDataR(response.data);
+                axios.get('https://inversiones-ellens-7b3ebbfa2822.herokuapp.com/clients/getClients')
+                    .then((response) => setClients(response.data));
             } catch (error) {
                 console.error("Error al cargar los datos", error);
             }
@@ -51,30 +53,6 @@ export function AllMovements() {
         navigate("/showDataMenu", { state });
     };
 
-    const getUserCodes = (e) => {
-        try {
-
-            const idClient = clientIdRef.current.value,
-                data = { idClient: idClient };
-
-            axios.post('https://inversiones-ellens-7b3ebbfa2822.herokuapp.com/codes/getClientCodes', data)
-                .then((response) => {
-                    let codesArray = [0, 0, 0, 0];
-                    let idCodesArray = [0, 0, 0, 0];
-
-                    for (let i = 0; i < response.data.length; i++) {
-                        codesArray[response.data[i].CodeType] = response.data[i].Code;
-                        idCodesArray[response.data[i].CodeType] = response.data[i].idAccountingCodes
-                    }
-                    console.log("codesArray", codesArray)
-                    setClientCodes(codesArray);
-                    setIdCodes(idCodesArray)
-                })
-        } catch (err) {
-            console.log(err)
-        }
-
-    };
 
     return (
         <Fragment >
@@ -85,33 +63,34 @@ export function AllMovements() {
                     <button className='back-button' onClick={goToDataMenu}>Atras</button>
 
                     <div>
+                        <br></br>
 
-                        <h2 className='title'>Tabla de Movimientos</h2>
-                        <h3 className='title' >Filtros</h3>
-                        <p>Cliente</p>
-                        <select id='clientSelect' className='selectFilter' ref={clientIdRef} onChange={getUserCodes}>
-                            {clients.map((client) => <option value={client.idClient}>{client.Name}</option>)}
-                        </select>
-                        <p>Fecha Creación</p>
-                        <input
-                            type="date"
-                            id="startDateSelect"
-                            className="selectFilter"
-                            placeholder="Fecha Creación"
-                        />
-                        <p>Fecha Vencimiento</p>
-                        <input
-                            type="date"
-                            id="endDateSelect"
-                            className="selectFilter"
-                            placeholder="Fecha Vencimiento"
-                        />
-                        <p>Estado</p>
-                        <select id='stateSelect' className='selectFilter'>
-                            <option value="0" defaultValue>Cancelado</option>
-                            <option value="1" defaultValue>Atrasado</option>
-                            <option value="2" defaultValue>En proceso</option>
-                        </select>
+                        <div className='filter-grid-2row'>
+                            <p>Cliente</p>
+                            <select id='clientSelect' className='selectFilter' ref={clientIdRef}>
+                                {clients.map((client) => <option value={client.idClient}>{client.Name}</option>)}
+                            </select>
+                            <p>Fecha Creación</p>
+                            <input
+                                type="date"
+                                id="startDateSelect"
+                                className="selectFilter"
+                                placeholder="Fecha Creación"
+                            />
+                            <p>Fecha Vencimiento</p>
+                            <input
+                                type="date"
+                                id="endDateSelect"
+                                className="selectFilter"
+                                placeholder="Fecha Vencimiento"
+                            />
+                            <p>Estado</p>
+                            <select id='stateSelect' className='selectFilter'>
+                                <option value="0" defaultValue>Cancelado</option>
+                                <option value="1" defaultValue>Atrasado</option>
+                                <option value="2" defaultValue>En proceso</option>
+                            </select>
+                        </div>
 
                         <table>
                             <thead>
--- src/pages/Tables/AllReceivables.jsx
+++ src/pages/Tables/AllReceivables.jsx
@@ -6,6 +6,7 @@ import axios from 'axios'
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { FiltersReceivablesFunction } from './FiltersReceivables';
+import { type } from '@testing-library/user-event/dist/type';
 
 export function AllReceivables() {
     const navigate = useNavigate(),
@@ -13,7 +14,7 @@ export function AllReceivables() {
         [clientCodes, setClientCodes] = useState([]),
         [clients, setClients] = useState([]),
         [idCodes, setIdCodes] = useState([]),
-        [dataR, setDataR] = useState([]);
+        [dataR, setDataR] = useState([])
 
     const { state } = useLocation();
 
@@ -24,7 +25,7 @@ export function AllReceivables() {
                     .then((response) => setDataR(response.data));
                 axios.get('https://inversiones-ellens-7b3ebbfa2822.herokuapp.com/clients/getClients')
                     .then((response) => setClients(response.data));
-                //FiltersReceivablesFunction();
+                
             } catch (error) {
                 console.error("Error al cargar los datos", error);
             }
@@ -57,63 +58,68 @@ export function AllReceivables() {
                     <button className='back-button' onClick={goToDataMenu}>Atras</button>
 
                     <div>
-                        <h2 className='title' >Cuentas por cobrar</h2>
-                        <h3 className='title' >Filtros</h3>
-                        <p>Cliente</p>
-                        <select id='clientSelect' className='selectFilter' ref={clientIdRef}>
-                            {clients.map((client) => <option value={client.idClient}>{client.Name}</option>)}
-                        </select>
-                        <p>Fecha Creación</p>
-                        <input
-                            type="date"
-                            id="startDateSelect"
-                            className="selectFilter"
-                            placeholder="Fecha Creación"
-                        />
-                        <p>Fecha Vencimiento</p>
-                        <input
-                            type="date"
-                            id="endDateSelect"
-                            className="selectFilter"
-                            placeholder="Fecha Vencimiento"
-                        />
-                        <p>Estado</p>
-                        <select id='stateSelect' className='selectFilter'>
-                            <option value="0" defaultValue>Cancelado</option>
-                            <option value="1" defaultValue>Atrasado</option>
-                            <option value="2" defaultValue>En proceso</option>
-                        </select>
-                        <br />
-                        <p>Rangos</p>
-                        <input className='selectFilter' type="number" id="min_value" name="min_value" placeholder='Mínimo' />
-                        <input className='selectFilter' type="number" id="max_value" name="max_value" placeholder='Máximo' />
-                        <table id='receivablesTable'>
-                            <thead>
-                                <tr>
-                                    <th>No operación</th>
-                                    <th>Fecha creación</th>
-                                    <th>Vencimiento</th>
-                                    <th>Cliente</th>
-                                    <th>Monto</th>
-                                    <th>Saldo</th>
-                                    <th>Estado</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {dataR.map((item, index) => (
-                                    <tr key={index}>
-                                        <td>{item.Operation}</td>
-                                        <td>{formatDate(item.StartDate)}</td>
-                                        <td>{formatDate(item.EndDate)}</td>
-                                        <td>{item.ClientName}</td>
-                                        <td>{item.Total}</td>
-                                        <td>{item.Balance}</td>
-                                        <td>{item.State}</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                        
+                        <br></br>
 
+                        <div className='filter-grid'>
+                            <p>Cliente</p>
+                            <select id='clientSelect' className='selectFilter' ref={clientIdRef}>
+                                {clients.map((client) => <option value={client.Name}>{client.Name}</option>)}
+                            </select>
+                            <p>Fecha Creación</p>
+                            <input
+                                type="date"
+                                id="startDateSelect"
+                                className="selectFilter"
+                                placeholder="Fecha Creación"
+                            />
+                            <p>Fecha Vencimiento</p>
+                            <input
+                                type="date"
+                                id="endDateSelect"
+                                className="selectFilter"
+                                placeholder="Fecha Vencimiento"
+                            />
+                            <p>Estado</p>
+                            <select id='stateSelect' className='selectFilter'>
+                                <option value="0" defaultValue>Cancelado</option>
+                                <option value="1" defaultValue>Atrasado</option>
+                                <option value="2" defaultValue>En proceso</option>
+                            </select>
+                            
+                            <p>Rangos</p>
+                            <input className='selectFilter' type="number" id="min_value" name="min_value" placeholder='Mínimo' />
+                            <input className='selectFilter' type="number" id="max_value" name="max_value" placeholder='Máximo' />
+                        </div>
+
+                        <div>
+                            <table id='receivablesTable'>
+                                <thead>
+                                    <tr>
+                                        <th>No operación</th>
+                                        <th>Fecha creación</th>
+                                        <th>Vencimiento</th>
+                                        <th>Cliente</th>
+                                        <th>Monto</th>
+                                        <th>Saldo</th>
+                                        <th>Estado</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {dataR.map((item, index) => (
+                                        <tr key={index}>
+                                            <td>{item.Operation}</td>
+                                            <td>{formatDate(item.StartDate)}</td>
+                                            <td>{formatDate(item.EndDate)}</td>
+                                            <td>{item.ClientName}</td>
+                                            <td>{item.Total}</td>
+                                            <td>{item.Balance}</td>
+                                            <td>{item.State}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
 
                     </div>
 
